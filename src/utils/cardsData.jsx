3e1469a7d3bc9ce9bdfa5f68bd2804fdf3cd4cc4
Martin Conti <matin.a.conti2014@gmail.com>
2023-12-21 19:00:49 -0300

    img display

--- src/utils/cardsData.jsx
+++ src/utils/cardsData.jsx
@@ -1,13 +1,13 @@
 export default [
   {
-    img: 'calculator.jpeg',
+    img: './calculator.jpeg',
     title: 'Calculator',
     description: 'This is a calculator app, using Javascript, React and Vite',
     website: 'https://vite-calculator-theta.vercel.app/',
     github: 'https://github.com/MARTINANDRESCONTI/vite-calculator'
   },
   {
-    img: 'weather.jpeg',
+    img: './weather.jpeg',
     title: 'Weather App',
     description: 'This is a calculator app, using Javascript, React and Vite',
     website: 'https://weather-app-vite-phi.vercel.app/',
